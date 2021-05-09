6ba6b9d75fc1972bc564d4b9d6f6aaaeabc534ea
TuanAnh2017 <tuananh24987@gmail.com>
2021-05-09 09:53:26 +0700

    Update_Repository

---
+++ TeduShop.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IOrderRepository
+    {
+    }
+
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(DbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ TeduShop.Data/Repositories/VisitorStatisticRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IVisitorStatistic
+    {
+
+    }
+    public class VisitorStatisticRepository : RepositoryBase<VisitorStatistic>, IVisitorStatistic
+    {
+        public VisitorStatisticRepository(DbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
